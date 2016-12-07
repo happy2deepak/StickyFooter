fda4e44a0dac5be6472b79c123e9e14cbaea1606
Deepak Yadav <happy2deepak@users.noreply.github.com>
2016-12-07 19:27:39 +0530

    jQuery for sticky footer
    Required jQuery for handling the footer-height and html padding on resize and page load.

---
+++ sticky-footer.js
@@ -0,0 +1,8 @@
+// Include Jquery before this snippet.
+$(document).ready(function(){
+	$(window).resize(function(){
+	$("html").css("padding-bottom",$("footer").outerHeight())
+		
+	}),
+	$("html").css("padding-bottom",$("footer").outerHeight());
+});
